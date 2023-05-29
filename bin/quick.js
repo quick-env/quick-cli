67866c9eb2f54efd3b872711285bdb066a6bc378
liya <liya@zuoyebang.com>
2023-05-29 10:16:28 +0800

    feat: add action

--- bin/quick.js
+++ bin/quick.js
@@ -7,18 +7,40 @@ import figlet from 'figlet';
 const program = new Command();
 
 program.version(pkg.version);
+program
+  .command('init <project-name>')
+  .description('初始化项目模板')
+  .action((cmd) => {
+    console.log(`...args --->`, cmd);
+  });
+
+program
+  .command('add')
+  .description('安装配置文件')
+  .action(() => {});
+
+program
+  .command('tlist')
+  .description('查看已支持的模板列表')
+  .action(() => {});
+
+program
+  .command('clist')
+  .description('查看可加载的配置列表')
+  .action(() => {});
+
 
 program.parse(process.argv);
 if (!program.args.length) {
   console.log(chalk.red(`\n`));
   console.log(
     figlet.textSync('quick cli', {
-      font: 'Ghost',
       horizontalLayout: 'default',
       verticalLayout: 'default',
       width: 80,
       whitespaceBreak: true,
     })
   );
+  console.log(chalk.red(`\n`));
   program.help();
 }
