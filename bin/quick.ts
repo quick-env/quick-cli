ef532833a05d6083b277d767f51f03e481a23fd2
liya <liya@zuoyebang.com>
2023-09-11 12:02:51 +0800

    feat: update text

--- bin/quick.ts
+++ bin/quick.ts
@@ -1,89 +1,91 @@
 #! /usr/bin/env node
-import ora from 'ora';
-import chalk from 'chalk';
-import inquirer from 'inquirer';
-import figlet from 'figlet';
-import pkg from '../package.json';
-import { Command } from 'commander';
-import QuickInit from '../core/init';
-import quickAddConfig from '../core/add';
-import { LINT_PROMPT } from '../prompt/lint.prompt';
-const program = new Command();
+import ora from 'ora'
+import chalk from 'chalk'
+import inquirer from 'inquirer'
+import figlet from 'figlet'
+import pkg from '../package.json'
+import { Command } from 'commander'
+import QuickInit from '../core/init'
+import quickAddConfig from '../core/add'
+import { LINT_PROMPT } from '../prompt/lint.prompt'
+const program = new Command()
 
-program.version(pkg.version);
-
-program
-  .command('init <project-name>')
-  .description('初始化项目模板')
-  .action((name) => {
-    const init = new QuickInit(name);
-    init._checkProject();
-    console.log(`name --->`, name);
-  });
+program.version(pkg.version)
 
 program
-  .command('add')
-  .description('添加配置文件')
-  .action(async () => {
-    inquirer.prompt(LINT_PROMPT).then((response: {lintConfig: string[]}) => {
-      const { lintConfig } = response;
-      const hasNone = lintConfig.includes('none');
-      if (hasNone) {
-        console.log(chalk.red(`选项包含none选项, 暂不配置`));
-        console.log(chalk.green(`${JSON.stringify(lintConfig)}`));
-        process.exit(1);
-      }
-      lintConfig.map((conf: string) => quickAddConfig._download(conf));
-    });
-  });
+	.command('init <project-name>')
+	.description('初始化项目模板')
+	.action((name) => {
+		const init = new QuickInit(name)
+		init._checkProject()
+		console.log(`name --->`, name)
+	})
 
 program
-  .command('template')
-  .description('查看已支持的模板列表')
-  .action(() => {
-    console.log(
-      chalk.magentaBright(
-        `\n`,
-        `\b Vue3中后台开发模板\n`,
-        `Vue3移动端开发模板\n`,
-        `Vue低代码工程开发模板\n`,
-        `Electron桌面客户端开发模板\n`,
-        `组件开发开发模板\n`,
-        `Nest工程开发模板\n`,
-        `React低代码工程开发模板\n`
-      )
-    );
-  });
+	.command('add')
+	.description('添加工程配置文件')
+	.action(async () => {
+		inquirer.prompt(LINT_PROMPT).then((response: { lintConfig: string[] }) => {
+			const { lintConfig } = response
+			const hasNone = lintConfig.includes('none')
+			if (hasNone) {
+				console.log(chalk.red(`选项包含none选项, 暂不配置`))
+				console.log(chalk.green(`${JSON.stringify(lintConfig)}`))
+				process.exit(1)
+			}
+			lintConfig.map((conf: string) => quickAddConfig._download(conf))
+		})
+	})
 
 program
-  .command('config')
-  .description('查看可加载的配置列表')
-  .action(() => {
-    console.log(
-      chalk.hex('#FFA500')(
-        `\n`,
-        `\b eslint\n`,
-        `stylelint\n`,
-        `commitlint\n`,
-        `husky + lint-stage\n`,
-        `webpack构建配置\n`,
-        `rollup构建配置\n`,
-        `vite构建配置\n`
-      )
-    );
-  });
+	.command('template')
+	.description('查看开发模板列表')
+	.action(() => {
+		console.log(
+			chalk.cyanBright(
+				`\n`,
+				`Vue3中后台开发模板\n`,
+				`Vue3移动端开发模板\n`,
+				`Vue组件开发开发模板\n`,
+				`NestJS开发模板\n`,
+				`React中后台开发模板\n`,
+				`React移动端开发模板\n`,
+				`Electron桌面端开发模板\n`
+			)
+		)
+	})
 
-program.parse(process.argv);
+program
+	.command('config')
+	.description('查看工程化列表')
+	.action(() => {
+		console.log(
+			chalk.hex('#FFA500')(
+				`\n`,
+				`husky\n`,
+				`postcss\n`,
+				`eslint\n`,
+				`prettier\n`,
+				`commitlint\n`,
+				`lint-staged\n`,
+				`webpack构建配置\n`,
+				`rollup构建配置\n`,
+				`vite构建配置\n`
+			)
+		)
+	})
+
+program.parse(process.argv)
 if (!program.args.length) {
-  console.log(chalk.red(`\n`));
-  console.log(
-    figlet.textSync('quick cli', {
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-      width: 80,
-      whitespaceBreak: true,
-    })
-  );
-  console.log(chalk.red(`\n`));
-  program.help();
+	console.log(chalk.red(`\n`))
+	console.log(
+		figlet.textSync('quick cli', {
+			horizontalLayout: 'default',
+			verticalLayout: 'default',
+			width: 80,
+			whitespaceBreak: true,
+		})
+	)
+	console.log(chalk.red(`\n`))
+	program.help()
 }
