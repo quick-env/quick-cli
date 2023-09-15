85f7e5e1f488c486aff21f31974afdf33b8fcd08
liya <liya@zuoyebang.com>
2023-09-15 18:04:13 +0800

    feat: 更新配置

--- constant/download.ts
+++ constant/download.ts
@@ -2,11 +2,11 @@
  * @Author: liya
  * @Date: 2023-09-15 17:58:38
  * @LastEditors: liya
- * @LastEditTime: 2023-09-15 17:59:42
+ * @LastEditTime: 2023-09-15 18:03:45
  * @Description: 下载配置
  */
 export const DOWNLOAD_CONFIG = {
   USER: 'quick-env',
   REPO: 'quick-config',
-  BRANCH: 'master'
+  BRANCH: 'main'
 }
